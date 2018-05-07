9c620f3714c0644160093903a1283405ebc4c4e9
BillGatesNephew <wlkrwilletts@gmail.com>
2018-05-06 22:48:29 -0400

    Started work on accordion for editing tags.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_tags
   protect_from_forgery with: :exception
+
+  def set_current_tags 
+    @tags = Tag.all
+  end 
+  
 end
