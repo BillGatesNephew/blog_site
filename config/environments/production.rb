9aa577d6b8ae583bff8da4368ff9af8c7cb7b304
BillGatesNephew <wlkrwilletts@gmail.com>
2018-05-13 13:49:32 -0400

    Trying to get assets to load.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
-
+  config.serve_static_assets = true
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
